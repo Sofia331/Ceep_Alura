84b2a9ef13fedc75658a20fcc6168371b4e1a17f
Sofia Mara <62350286+Sofia331@users.noreply.github.com>
2022-01-14 11:29:06 -0300

    >.<

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,16 +1,38 @@
 const criarTarefa = (evento) => { 
-
     evento.preventDefault()
-
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log( valor  )
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConcluir())
+    lista.appendChild(tarefa)
     input.value = " "
 }
+
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
 
+const BotaoConcluir = () => {
+    const botaoConcluir = document.createElement('button')
+    botaoConcluir.classList.add('check-button')
+    botaoConcluir.innerText = 'concluir'
+    botaoConcluir.addEventListener('click', concluirTarefa)
 
+    return botaoConcluir
+}
 
+const concluirTarefa = (evento) => {
+    const botaoConcluir = evento.target
+
+    const tarefaCompleta = botaoConcluir.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
